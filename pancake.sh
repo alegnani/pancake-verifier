9fc669482b7943a986b1d029aad1659e54fcfed6
Alessandro Legnani <alegnani@ethz.ch>
2024-08-16 13:27:32 +1000

    created workspace

--- pancake.sh
+++ pancake.sh
@@ -1,3 +1,3 @@
 set -e
 cat $@ | cpp -P | cake --pancake > pancake.S
-gcc -DEVAL -o pancake.out pancake.S ./cake-x64-64/basis_ffi.c && rm pancake.S
+gcc -DEVAL -o pancake.out pancake.S ../cake-x64-64/basis_ffi.c && rm pancake.S
