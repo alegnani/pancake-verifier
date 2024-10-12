b3f238dced220add0e4d97ede74fe043421483c4
Alessandro Legnani <alegnani@ethz.ch>
2024-10-12 08:50:13 +0200

    shared loads size postcondition, alignment checks, code cleanup

--- pancake2viper/src/viper_prelude/shared_mem.rs
+++ pancake2viper/src/viper_prelude/shared_mem.rs
@@ -5,7 +5,20 @@ use crate::utils::ViperUtils;
 pub fn create_shared_mem_methods(ast: AstFactory<'_>) -> Vec<Method<'_>> {
     let address = ast.new_var("address", ast.int_type());
     let value = ast.new_var("value", ast.int_type());
+    let upper = ast.new_var("upper", ast.int_type());
     let store = ast.method("shared_store", &[address.0, value.0], &[], &[], &[], None);
-    let load = ast.method("shared_load", &[address.0], &[value.0], &[], &[], None);
+
+    let post = ast.and(
+        ast.le_cmp(ast.int_lit(0), value.1),
+        ast.lt_cmp(value.1, upper.1),
+    );
+    let load = ast.method(
+        "shared_load",
+        &[address.0, upper.0],
+        &[value.0],
+        &[],
+        &[post],
+        None,
+    );
     vec![load, store]
 }
