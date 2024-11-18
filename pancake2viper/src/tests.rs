0a2d18bf390411198654ece7c14165e9c9d3a754
Alessandro Legnani <alegnani@ethz.ch>
2024-11-18 22:35:21 +1100

    added support for tests using a model

--- pancake2viper/src/tests.rs
+++ pancake2viper/src/tests.rs
@@ -1,39 +1,22 @@
 use std::env;
 
-use lazy_static::lazy_static;
-use utils::{ConstEval, EncodeOptions, Mangleable, Mangler, ProgramToViper};
+use app::App;
 
 use super::*;
 
-lazy_static! {
-    static ref VIPER: Viper = Viper::new_with_args(&env::var("VIPER_HOME").unwrap(), vec![]);
+fn verify_file(path: &str) -> anyhow::Result<()> {
+    let mut app = App::default();
+    app.code = std::fs::read_to_string(path)?;
+    app.verify = true;
+
+    Ok(())
 }
 
-fn verify_file(path: &str) -> anyhow::Result<()> {
-    let cake = env::var("CAKE_ML").unwrap_or("cake".into());
-
-    // Parse Pancake program
-    let program = pancake::Program::parse_file(path, &cake)?;
-    let mut program: ir::Program = program.try_into()?;
-    println!("{:?}", program);
-    let mut mangler = Mangler::default();
-    program.mangle(&mut mangler)?;
-    println!("{:?}", program);
-    let ctx = program.resolve_types()?;
-    println!("Resolved types!: {:?}", ctx);
-    let program = program.const_eval(&EncodeOptions::default());
-    // Create Viper context
-    // let viper = Viper::new_with_args(&viper_home, vec![]);
-    let ver_ctx = VIPER.attach_current_thread();
-    let ast_factory = ver_ctx.new_ast_factory();
-    let mut verifier = ver_ctx.new_verifier_with_default_smt_and_extra_args(
-        viper::VerificationBackend::Silicon,
-        vec!["--logLevel=OFF".into()],
-    );
-    // Tranpile to Viper and verify
-    let program = program.to_viper(ctx, ast_factory, EncodeOptions::default())?;
-    let res = verifier.verify(program);
-    assert!(res.is_success(), "Verification error: {:?}", res);
+fn verify_file_model(path: &str, model: &str) -> anyhow::Result<()> {
+    let mut app = App::default();
+    app.code = std::fs::read_to_string(path).unwrap();
+    app.model = Some(std::fs::read_to_string(model)?);
+    app.verify = true;
 
     Ok(())
 }
